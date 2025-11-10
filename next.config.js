032f319d2648fcf593ec9d65d79b73aee9cb0545
nlavidas <nikolaos.lavidas@gmail.com>
2025-11-10 03:29:45 +0200

    Fix: Add basePath for GitHub Pages subdirectory deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/Delphi2025',
+  assetPrefix: '/Delphi2025',
   images: {
     unoptimized: true,
   },
