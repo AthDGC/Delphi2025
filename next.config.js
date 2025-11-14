91950477dfef2621866e1bd63e0ba66933b8d609
nlavidas <nikolaos.lavidas@gmail.com>
2025-11-14 22:25:22 +0200

    Fix basePath to match repository name Delphi2025

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/delphi-workshop-2025',
-  assetPrefix: '/delphi-workshop-2025',
+  basePath: '/Delphi2025',
+  assetPrefix: '/Delphi2025',
   images: {
     unoptimized: true,
   },
